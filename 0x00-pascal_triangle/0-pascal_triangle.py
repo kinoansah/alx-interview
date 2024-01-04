92c54ff8e82643c01efeef6fa74b5174a7e0306d
kinoansah <kinoansah@gmail.com>
2024-01-04 03:13:33 -0800

    Pascal Triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,14 @@
+def pascal_triangle(n):
+    """
+    Generate Pascal's Triangle up to the given number of rows (n).
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1] * (i + 1) for i in range(n)]
+
+    for i in range(2, n):
+        for j in range(1, i):
+            triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+
+    return triangle
